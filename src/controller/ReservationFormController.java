77ed4c26bf52c0c05657d0e6df5cfb581bd5e625
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-04-12 23:23:46 +0530

    change Reservation UI

--- src/controller/ReservationFormController.java
+++ src/controller/ReservationFormController.java
@@ -1,13 +1,20 @@
 package controller;
 
 import com.jfoenix.controls.JFXTextField;
+import dto.RoomsDTO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import service.custom.RoomsService;
+import service.custom.impl.RoomsServiceImpl;
 import util.Navigation;
 import util.Routes;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ReservationFormController {
 
@@ -21,12 +28,17 @@ public class ReservationFormController {
     public TableColumn colStatus;
     public Label lblRoomId;
     public ComboBox cmbStudentId;
+    public AnchorPane pane;
 
-    public void btnBackOnAction(ActionEvent actionEvent) {
+    private final RoomsService roomsService = new RoomsServiceImpl();
+    public ComboBox cmbRoomType;
+    public Label lblRoomType;
+    public ComboBox cmbRoomId;
+
+    public void btnBackOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.MAIN_FORM,pane);
     }
 
-    public void txtReservationIdOnAction(ActionEvent actionEvent) {
-    }
 
     public void btnAddOnAction(ActionEvent actionEvent) {
     }
@@ -40,9 +52,21 @@ public class ReservationFormController {
     public void btnDeleteOnAction(ActionEvent actionEvent) {
     }
 
-    public void txtRoomTypeOnAction(ActionEvent actionEvent) {
+    public void cmbStudentIdOnAction(ActionEvent actionEvent) {
+
     }
 
-    public void cmbStudentIdOnAction(ActionEvent actionEvent) {
+    public void cmbRoomIdOnAction(ActionEvent actionEvent) {
+        try {
+            ArrayList<RoomsDTO> allRooms = roomsService.getAllRooms();
+            ObservableList observableList = FXCollections.observableArrayList();
+            for (RoomsDTO r:allRooms) {
+                observableList.add(r.getRoomId());
+                cmbRoomId.setItems(observableList);
+
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
